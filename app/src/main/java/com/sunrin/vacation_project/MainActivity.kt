645e0e462af9f1f560b2f0e9a448ef9e6ef39e9c
dlfjstizlzl <dlfjstilzlzl2007@gmail.com>
2023-07-31 04:04:00 +0900

    1

--- app/src/main/java/com/sunrin/vacation_project/MainActivity.kt
+++ app/src/main/java/com/sunrin/vacation_project/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.sunrin.vacation_project
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nextBtn:Button = findViewById(R.id.nextBtn)
+        nextBtn.setOnClickListener {
+            var intent = Intent(this, mapActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/sunrin/vacation_project/mapActivity.kt
@@ -0,0 +1,28 @@
+package com.sunrin.vacation_project
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.sunrin.vacation_project.databinding.ActivityMainBinding
+import com.sunrin.vacation_project.databinding.ActivityMapBinding
+import net.daum.android.map.MapView
+
+
+class mapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMapBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val mapView = MapView(this)
+        binding.clKakaoMapView.addView(mapView)
+
+    }
+}
