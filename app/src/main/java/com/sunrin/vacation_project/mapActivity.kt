7be380837dc4896a8c332810c5ff9c5c1c672890
dlfjstizlzl <dlfjstilzlzl2007@gmail.com>
2023-08-08 23:56:14 +0900

    커스텀 마커

--- app/src/main/java/com/sunrin/vacation_project/mapActivity.kt
+++ app/src/main/java/com/sunrin/vacation_project/mapActivity.kt
@@ -2,19 +2,16 @@ package com.sunrin.vacation_project
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.sunrin.vacation_project.databinding.ActivityMainBinding
 import com.sunrin.vacation_project.databinding.ActivityMapBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
 
 
 class mapActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMapBinding
+    private lateinit var binding: ActivityMapBinding    // 뷰 바인딩
+    private lateinit var mapView : MapView              // 카카오 지도 뷰
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,5 +27,18 @@ class mapActivity : AppCompatActivity() {
             val intent = Intent(this, ScannerActivity::class.java)
             startActivity(intent)
         }
+
+        val marker = MapPOIItem()
+        marker.apply {
+            itemName = "서울시청"   // 마커 이름
+            mapPoint = MapPoint.mapPointWithGeoCoord(37.5666805, 126.9784147)   // 좌표
+            markerType = MapPOIItem.MarkerType.CustomImage          // 마커 모양 (커스텀)
+            customImageResourceId = R.drawable.custom_marker               // 커스텀 마커 이미지
+            selectedMarkerType = MapPOIItem.MarkerType.CustomImage  // 클릭 시 마커 모양 (커스텀)
+            customSelectedImageResourceId = R.drawable.custom_marker2       // 클릭 시 커스텀 마커 이미지
+            isCustomImageAutoscale = false      // 커스텀 마커 이미지 크기 자동 조정
+            setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
+        }
+        mapView.addPOIItem(marker)
     }
 }
