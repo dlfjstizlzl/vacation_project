ec214640441f98a7b817e946fe78cae1efd3db28
dlfjstizlzl <dlfjstilzlzl2007@gmail.com>
2023-08-16 22:44:03 +0900

    Fastapi 통신 기능

--- app/src/main/java/com/sunrin/vacation_project/mapActivity.kt
+++ app/src/main/java/com/sunrin/vacation_project/mapActivity.kt
@@ -2,11 +2,16 @@ package com.sunrin.vacation_project
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.auth.User
 import com.sunrin.vacation_project.databinding.ActivityMapBinding
 import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class mapActivity : AppCompatActivity() {
@@ -40,5 +45,23 @@ class mapActivity : AppCompatActivity() {
             setCustomImageAnchor(0.5f, 1.0f)    // 마커 이미지 기준점
         }
         mapView.addPOIItem(marker)
+
+        RetrofitClass.getInstance()?.getUserPage(1234)?.enqueue(object: Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if(response.isSuccessful){
+                    // 정상적으로 통신이 성고된 경우
+                    var result: User? = response.body()
+                    Log.d("YMC", "onResponse2 성공: " + result?.toString());
+                }else{
+                    // 통신이 실패한 경우(응답코드 3xx, 4xx 등)
+                    Log.d("YMC", "onResponse2 실패")
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                // 통신 실패 (인터넷 끊킴, 예외 발생 등 시스템적인 이유)
+                Log.d("YMC", "onFailure2 에러: " + t.message.toString());
+            }
+        })
     }
 }
